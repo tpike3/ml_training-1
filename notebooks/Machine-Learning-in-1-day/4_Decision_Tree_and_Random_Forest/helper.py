2018c9a941db6981a9bfdb1f1cce0f0887d083cc
tpike3 <tpike3@gmu.edu>
2021-09-08 15:12:23 -0400

    updated ML in 1 day

--- notebooks/Machine-Learning-in-1-day/4_Decision_Tree_and_Random_Forest/helper.py
+++ notebooks/Machine-Learning-in-1-day/4_Decision_Tree_and_Random_Forest/helper.py
@@ -11,7 +11,7 @@ def visualize_tree(estimator, X, y, boundaries=True,
     
     # Plot the training points
     ax.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap='viridis',
-               clim=(y.min(), y.max()), zorder=3)
+               zorder=3)
     ax.axis('tight')
     ax.axis('off')
     if xlim is None:
@@ -30,7 +30,7 @@ def visualize_tree(estimator, X, y, boundaries=True,
     Z = Z.reshape(xx.shape)
     contours = ax.contourf(xx, yy, Z, alpha=0.3,
                            levels=np.arange(n_classes + 1) - 0.5,
-                           cmap='viridis', clim=(y.min(), y.max()),
+                           cmap='viridis', 
                            zorder=1)
 
     ax.set(xlim=xlim, ylim=ylim)
